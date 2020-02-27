d5d43c14aee288c550df7bcdd4966f4b727894e1
greattechpk <great.techpk@gmail.com>
2020-02-27 17:24:53 -0500

    added game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,10 +1,25 @@
-console.log("Up and running!");
 
-let cardOne = "Queen";
-let cardTwo = "Queen";
-let cardThree = "King";
-let cardFour = "King";
+let cards = ["Queen","Queen","King","King"];
+let cardsInPlay = [];
 
+let cardOne = cards[0];
+let cardTwo = cards[2];
+
+
+cardsInPlay.push(cardOne);console.log()
+cardsInPlay.push(cardTwo);
+
+
+
+console.log(cardsInPlay);
+
+console.log("User flipped " + cardTwo);
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
 
+if(cardsInPlay.length === 2){
+    if (cardsInPlay[0]===cardsInPlay[1]){
+        alert("A pair was found!");
+    }else{
+        alert("Sorry try again!");
+    }
+}
