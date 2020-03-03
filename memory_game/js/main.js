c8f3ebda1204dcefaace11f0b39a10ea0046bfff
greattechpk <great.techpk@gmail.com>
2020-03-02 22:09:55 -0500

    Created Objects for Each Card

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,5 +1,30 @@
 
-let cards = ["Queen","Queen","King","King"];
+//objects starts here for now
+
+const cards = [
+
+    {
+        rank: "queen",
+        suit: "hearts",
+        cardImage: "images/queen-of-hearts.png"
+    },
+    {
+        rank: "queen",
+        suit: "diamonds",
+        cardImage: "images/queen-of-diamonds.png"
+    },
+    {
+        rank: "king",
+        suit: "hearts",
+        cardImage: "images/king-of-hearts.png"
+    },
+    {
+        rank: "king",
+        suit: "diamonds",
+        cardImage: "images/king-of-diamonds.png"
+    }
+]
+
 let cardsInPlay = [];
 
 function checkForMatch(){
@@ -16,19 +41,18 @@ function checkForMatch(){
 
 function flipCard(cardId){
 
-    console.log("User flipped " + cards[cardId]);
+    console.log("User flipped " + cards[cardId].rank);
 
-    cardsInPlay.push(cards[cardId]);
+    cardsInPlay.push(cards[cardId].rank);
 
     checkForMatch();
 }
 
 flipCard(0);
-
+console.log(cards[0].cardImage +" " + cards[0].suit)
 flipCard(1);
-
+console.log(cards[1].cardImage +" " + cards[1].suit)
 
 
 console.log(cardsInPlay);
-
 
